7cffc2a9fd71ce06764e98384053966ef97b233b
dit3456 <74191761+dit3456@users.noreply.github.com>
2021-06-22 10:23:02 +0200

    Update Locator.Installer.js

--- Javascript/Locator.Installer.js
+++ Javascript/Locator.Installer.js
@@ -69,7 +69,7 @@ Locator.Installer = (function (){
         AllInstallers.features = [];
 
         return new Promise(function(resolve, reject){
-            fetch('../CSV_Files/Installers_Database.csv')
+            fetch('https://dit3456.github.io/instshowrm-locator/CSV_Files/Installers_Database.csv')
                 .then(response => response.text())
                 .then(data => {
                     parsedData = Papa.parse(data, {
